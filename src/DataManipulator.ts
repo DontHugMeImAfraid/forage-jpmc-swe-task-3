cfc0a1c0ef0c287b1302274c8f37b136c525cc8d
fortunebowie@gmail.com <fortunebowie@gmail.com>
2023-09-26 13:07:54 +0100

    Bug Fixes and Visual
    DataManipulator. ts:
    - Removed duplicate return statements
    - fixed x axis display issues
    Graph.tsx:
    - added spacing for attribute names

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -12,13 +12,12 @@ export interface Row {
 
 
 export class DataManipulator {
-  static generateRow(serverRespond: ServerRespond[]) {
+  static generateRow(serverRespond: ServerRespond[]): Row {
     const priceABC = (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price) / 2;
     const priceDEF = (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price) / 2;
     const ratio = priceABC / priceDEF;
     const upperBound = 1 + 0.05;
     const lowerBound = 1 - 0.05;
-    return serverRespond.map((el: any) => {
       return {
         price_abc: priceABC,
         price_def: priceDEF,
@@ -29,6 +28,6 @@ export class DataManipulator {
         lower_bound: lowerBound,
         trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined,
       };
-    })
+    
   }
 }
